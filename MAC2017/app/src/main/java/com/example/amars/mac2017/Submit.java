7f667969f87416ead9f36d65d8a96aed30fdbb06
Havorax <havorax@gmail.com>
2017-04-29 05:39:32 -0700

    submit reroutes
    b

--- MAC2017/app/src/main/java/com/example/amars/mac2017/Submit.java
+++ MAC2017/app/src/main/java/com/example/amars/mac2017/Submit.java
@@ -1,7 +1,10 @@
 package com.example.amars.mac2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Submit extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Submit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+
+        Button events = (Button) findViewById(R.id.submit_event);
+        events.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMain();
+            }
+        });
+
+    }
+
+    private void goToMain() {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
     }
 }
