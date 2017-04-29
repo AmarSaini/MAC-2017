ae462b02ab95448d1b239745528c8fccd3409641
Havorax <havorax@gmail.com>
2017-04-29 03:46:51 -0700

    haha
    b

--- MAC2017/app/src/main/java/com/example/amars/mac2017/SignUp.java
+++ MAC2017/app/src/main/java/com/example/amars/mac2017/SignUp.java
@@ -1,7 +1,10 @@
 package com.example.amars.mac2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button signUp = (Button) findViewById(R.id.sign_up);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+
+    }
+
+    private void goToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
